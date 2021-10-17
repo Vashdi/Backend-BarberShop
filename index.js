52abea4eeac53f1a671e938b618a94b2f9098c1d
Vashdi <62853009+Vashdi@users.noreply.github.com>
2021-10-18 00:54:36 +0300

    blah

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
 app.use(express.static('build'))
 app.use(express.json())
 
-app.use(cors())
+app.use(cors(corsOptions));
 
 const url = process.env.MONGODB_URI;
 
