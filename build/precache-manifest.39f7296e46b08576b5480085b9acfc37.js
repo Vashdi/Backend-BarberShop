96b367a0952ec1b0b7adbdcd3208c5ecb26c5052
Vashdi <Vashdi7002@gmail.com>
2022-05-23 16:10:35 +0300

    blah

--- build/precache-manifest.39f7296e46b08576b5480085b9acfc37.js
+++ build/precache-manifest.39f7296e46b08576b5480085b9acfc37.js
@@ -1,6 +1,5 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<<< HEAD:build/precache-manifest.39f7296e46b08576b5480085b9acfc37.js
     "revision": "d8eb856f632d9b05b3599934ab29b5e0",
     "url": "/index.html"
   },
@@ -23,30 +22,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "080b7ddc8f3c7aaf2027",
     "url": "/static/js/main.f311d5e8.chunk.js"
-========
-    "revision": "e1035eb940abb4a603c38d55cbced6dc",
-    "url": "/index.html"
-  },
-  {
-    "revision": "b50b00e54d5d1b611d66",
-    "url": "/static/css/2.3214fa31.chunk.css"
-  },
-  {
-    "revision": "0297d4274c92278a5057",
-    "url": "/static/css/main.faeb9a9b.chunk.css"
-  },
-  {
-    "revision": "b50b00e54d5d1b611d66",
-    "url": "/static/js/2.d758388e.chunk.js"
-  },
-  {
-    "revision": "b948d2a7727b83687a00ec53ef5267c0",
-    "url": "/static/js/2.d758388e.chunk.js.LICENSE.txt"
-  },
-  {
-    "revision": "0297d4274c92278a5057",
-    "url": "/static/js/main.85181372.chunk.js"
->>>>>>>> be38cd0d2ce3d6e80e4501f64dd6581d4ca86fa2:build/precache-manifest.6bb412cc8c11b2803616f188c19c68a7.js
   },
   {
     "revision": "27ef4e802eb02d9b1da1",
